c46067b67c68ce4a715274e8f3a43012ea41cbe8
kusstar <kussssss@outlook.com>
2025-08-17 03:51:24 +0800

    fix: typecheck

--- example80/src/components/useColorTheme.ts
+++ example80/src/components/useColorTheme.ts
@@ -1,8 +1,8 @@
 import { useColorScheme } from 'react-native';
 
-import { DefaultTheme, DarkTheme } from '@react-navigation/native';
+import { DefaultTheme, DarkTheme, type Theme } from '@react-navigation/native';
 
-export const useColorTheme = () => {
+export const useColorTheme = (): Theme => {
   const isDark = useColorScheme() === 'dark';
   return isDark ? DarkTheme : DefaultTheme;
 };
